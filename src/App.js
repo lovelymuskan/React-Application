410131853670d10b174cde922b75e0e86db11341
deepkumarlaha <deep.laha@indusnet.co.in>
2024-05-24 11:57:11 +0530

    edit App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-        Hello World by Deep Kumar 
+        Hello World
       </header>
     </div>
   );
